2580f99adb08018befb4b937989f797e26f65a3d
dlake00 <danlake00@gmail.com>
2021-11-29 09:32:44 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set CORS
+
+// Run when client connects
+io.on("connection", socket => {
+    console.log("Client ID " + socket.id + " has connected")
+
+    // Run when client disconnects
+    socket.on("disconnect", reason => {
+        console.log("Client ID " + socket.id + " has disconnected")
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
