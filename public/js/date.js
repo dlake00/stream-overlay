1f7c76e0f8d8d3e56feeeae921353d33f56da5ee
dan <danlake00@gmail.com>
2022-05-09 16:05:49 +0100

    date

--- public/js/date.js
+++ public/js/date.js
@@ -71,26 +71,14 @@ function currentDate() {
 
   let suffix;
   switch (day) {
-    case 1:
+    case 1: case 21: case 31:
       suffix = "st";
       break;
-    case 2:
+    case 2: case 22:
       suffix = "nd";
       break;
-    case 3:
+    case 3: case 23:
       suffix = "rd";
-      break;
-    case 21:
-      suffix = "st";
-      break;
-    case 22:
-      suffix = "nd";
-      break;
-    case 23:
-      suffix = "rd";
-      break;
-    case 31:
-      suffix = "st";
       break;
     default:
       suffix = "th";
